6e2d225c820ab463c1dc6322f9373173a288e05b
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2016-04-18 14:28:49 +0200

    Add GradientValue

--- src/ColorizeNode.js
+++ src/ColorizeNode.js
@@ -12,18 +12,19 @@
         return;
       }
       var image = this.getInput('Image') || new ImageData(512, 512);
-      var gradient = JSON.parse(this.getInput('Gradient') ||
-                                JSON.stringify([{offset: 0, color: 'black'}, {offset:1, color:'white'}]));
+      var gradient = this.getInput('Gradient') || new TextureGen.GradientValue(0, 0, 256, 0, [
+          {offset: 0, color: 'black'},
+          {offset: 1, color:'white'}
+      ]);
       var canvas = document.createElement('canvas');
       canvas.width = 256;
       canvas.height = 1;
       var ctx = canvas.getContext('2d');
-      var canvasGradient = ctx.createLinearGradient(0, 0, 256, 0);
-      for(var i = 0; i < gradient.length; i++) {
-        var colorStop = gradient[i];
-        canvasGradient.addColorStop(colorStop.offset, colorStop.color);
-      }
-      ctx.fillStyle = canvasGradient;
+      gradient.x0 = 0;
+      gradient.y0 = 0;
+      gradient.x1 = 256;
+      gradient.y1 = 0;
+      ctx.fillStyle = gradient.toCanvasStyle();
       ctx.fillRect(0, 0, 256, 1);
       this.imageData = texturegen.colorize(image, ctx.getImageData(0, 0, 256, 1));
       this.ctx.putImageData(this.imageData, 0, 0);
